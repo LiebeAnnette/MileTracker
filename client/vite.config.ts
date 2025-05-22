c3965110cc6fa40c40e57a47b3d7716ba7981c9e
LiebeAnnette <liebeannette@gmail.com>
2025-05-21 19:18:33 -0500

    updated vite.config.ts allowedHost for devDeploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // use this in main branch
-const allowedHost = "miletracker-client.onrender.com";
+// const allowedHost = "miletracker-client.onrender.com";
 
 // TODO: comment this out when pushing to main
-// const allowedHost = "miletracker-devclient.onrender.com";
+const allowedHost = "miletracker-devclient.onrender.com";
 
 export default defineConfig({
   plugins: [react()],

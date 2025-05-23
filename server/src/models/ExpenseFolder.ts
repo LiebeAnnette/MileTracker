5ac5da67666848f71ce3b11e09a8e65e65568d16
Stephen Schier <stephenjschier@gmail.com>
2025-05-22 20:51:38 -0500

    Created ExpenseFolder.ts

---
+++ server/src/models/ExpenseFolder.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const expenseItemSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        enum: ["Airfare", "Food", "Hotel", "Vehicle", "Miscellaneous"],
+        required: true,
+    },
+    description: String,
+    amount: { type: Number, required: true },
+});
+
+const expenseFolderSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    expenses: [expenseItemSchema],
+});
+
+const ExpenseFolder = mongoose.model("ExpenseFolder", expenseFolderSchema);
+export default ExpenseFolder;
